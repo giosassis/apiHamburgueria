6e2970f1f0b51727793479db12c15610945ca8bc
Giovana Assis <giovana.sant@hotmail.com>
2021-08-31 19:48:43 -0300

    fix: new config

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const config = require("./infra/config");
 const bancoDeDados = require("./infra/bancoDeDados");
 const Tabelas = require("./infra/tabelaPedido");
 const app = config();
+const cors = require("cors");
+const port = process.env.PORT;
 
-app.listen(3000, () => console.log("Servidor nasceu!"));
+app.listen(port, () => console.log("Servidor nasceu!"));
+app.use(cors());
 
 const tabela = new Tabelas();
 bancoDeDados.run(tabela.pedidos);
